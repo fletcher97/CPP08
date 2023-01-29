13ac8e8dc8c33337fcb37aa1452ce16c1f0d33a5
Miguel Gueifao <miguel.gueifao@gmail.com>
2023-01-29 19:54:27 +0100

    Changed tests

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -11,8 +11,9 @@
 void
 print(Span& s) {
 	// s.print();
-	std::cout << s[0] << std::endl;
-	std::cout << s[s.size() - 1] << std::endl;
+	std::cout << "elems: " << s.size() << "/" << s.max_size() << std::endl;
+	std::cout << "pos 0: " << s[0] << std::endl;
+	std::cout << "pos " << (s.size() - 1) << ": " << s[s.size() - 1] << std::endl;
 	std::cout << "Longest span: " << s.longestSpan() << std::endl;
 	std::cout << "Shortest span: " << s.shortestSpan() << std::endl;
 }
@@ -23,7 +24,7 @@ main() {
 	std::srand(std::time(NULL));
 
 	Span span(SIZE);
-	for (int i = 0; i < SIZE; i++) {
+	for (int i = 0; i < SIZE/2; i++) {
 		int ret = std::rand() % MOD;
 		span.addNumber(ret);
 	}
@@ -31,10 +32,14 @@ main() {
 	print(span);
 
 	std::vector<int> vec;
-	for (int i = 0; i < SIZE; i++)
+	for (int i = 0; i < SIZE/2; i++)
 		vec.push_back(i);
 
-	span.addNumber<std::vector<int> >(vec.begin(), vec.end());
+	try {
+		span.addNumber<std::vector<int> >(vec.begin(), vec.end());
+	} catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
 
 	std::cout << std::endl;
 
