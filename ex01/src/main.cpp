7229bc050f07a3ad8f52a860d56516139ac08aea
Miguel Gueifao <miguel.gueifao@gmail.com>
2023-02-03 00:18:28 +0100

    Changed tests

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -5,41 +5,25 @@
 
 #include "Span.hpp"
 
-#define SIZE 10000
-#define MOD 99999999
+void
+theirTest() {
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+}
 
 void
-print(Span& s) {
-	// s.print();
-	std::cout << "elems: " << s.size() << "/" << s.max_size() << std::endl;
-	std::cout << "pos 0: " << s[0] << std::endl;
-	std::cout << "pos " << (s.size() - 1) << ": " << s[s.size() - 1] << std::endl;
-	std::cout << "Longest span: " << s.longestSpan() << std::endl;
-	std::cout << "Shortest span: " << s.shortestSpan() << std::endl;
 }
 
 int
 main() {
 
-	std::srand(std::time(NULL));
-
-	Span span(SIZE);
-	for (int i = 0; i < SIZE/2; i++) {
-		int ret = std::rand() % MOD;
-		span.addNumber(ret);
-	}
-
-	print(span);
-
-	std::vector<int> vec;
-	for (int i = 0; i < SIZE/2; i++)
-		vec.push_back(i);
-
-	try {
-		span.addNumber<std::vector<int> >(vec.begin(), vec.end());
-	} catch (std::exception& e) {
-		std::cout << e.what() << std::endl;
-	}
+	theirTest();
 
 	std::cout << std::endl;
 
